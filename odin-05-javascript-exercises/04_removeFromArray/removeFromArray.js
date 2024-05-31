7bc4800b070e21f0687348dc8b0b7bcf866f0285
Scott Broyles <scott_broyles@selinc.com>
2024-05-31 16:47:37 -0700

    Solution for 04_removeFromArray

--- odin-05-javascript-exercises/04_removeFromArray/removeFromArray.js
+++ odin-05-javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function( array, ...toRemove ) {
+  
+  const newArray = [];
+  
+  for( let i = 0; i < array.length; i++ ) {
+    // for( let j = 0; j < toRemove.length; j++ ) {
+      if( !toRemove.includes( array[i] ) ) {
+        newArray.push( array[i] );
+      }
+    // }
+  }
+  return newArray;
 };
 
 // Do not edit below this line
