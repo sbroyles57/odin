7bf3fcc2a0f6d09670be0641f22f164e1d5215d2
Scott Broyles <scott_broyles@selinc.com>
2024-06-04 10:30:55 -0700

    Solution for 05_sumAll

--- odin-05-javascript-exercises/05_sumAll/sumAll.js
+++ odin-05-javascript-exercises/05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function( start, end ) {
 
+    let result = 0;
+
+    if( !( Number.isInteger( start ) && start > 0 ) ) {
+      return "ERROR";
+    }
+
+    if( !( Number.isInteger( end ) && end > 0 ) ) {
+      return "ERROR";
+    }
+
+    let tmpStart = ( start > end ) ? end : start; 
+    let tmpEnd = ( start < end ) ? end : start;
+
+    for( let i = tmpStart; i <= tmpEnd; i++ ) {
+      result += i; 
+    }
+
+    return result;
 };
 
 // Do not edit below this line
