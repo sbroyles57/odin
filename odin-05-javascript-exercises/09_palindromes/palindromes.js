755cee47d575828eb43cf947f78f772745e8fe8e
Scott Broyles <scott_broyles@selinc.com>
2024-06-13 17:09:37 -0700

    Solution for 09_palindromes

--- odin-05-javascript-exercises/09_palindromes/palindromes.js
+++ odin-05-javascript-exercises/09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function ( pal ) {
+    const pat = /[^A-Za-z0-9]/g;
+    let lowerPal = pal.toLowerCase().replace( pat, '' );
+    let revPal = lowerPal.split( '' ).reverse().join( '' );
+    return revPal === lowerPal;
 };
 
 // Do not edit below this line
