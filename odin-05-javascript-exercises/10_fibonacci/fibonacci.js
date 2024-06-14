46eac63cb21d32d1d1661763f87e5dfd2d2e4f2e
Scott Broyles <scott_broyles@selinc.com>
2024-06-14 09:20:58 -0700

    Solution for 10_fibonacci

--- odin-05-javascript-exercises/10_fibonacci/fibonacci.js
+++ odin-05-javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function( f ) {
+  const fibSeq = [ 1, 1 ];
+  let fibEnd = +f;
 
+  if ( fibEnd < 0 ) {
+    return "OOPS";
+  }
+  
+  if( 0 === fibEnd ) {
+    return 0;
+  }
+
+  if( 1 === fibEnd || 2 === fibEnd ) {
+    return 1;
+  }
+
+  for( let i = 2; i < fibEnd; i++ ) {
+    fibSeq.push( fibSeq[ i-2 ] + fibSeq[ i-1 ] );
+  }
+
+  return fibSeq[ fibSeq.length - 1 ];
 };
 
 // Do not edit below this line
