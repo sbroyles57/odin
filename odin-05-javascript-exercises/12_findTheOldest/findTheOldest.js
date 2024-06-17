8f1189115c0405ab36b7dda64350e7b671c30348
Scott Broyles <scott_broyles@selinc.com>
2024-06-17 12:01:57 -0700

    Solution for 12_findTheOldest

--- odin-05-javascript-exercises/12_findTheOldest/findTheOldest.js
+++ odin-05-javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const getAge = function( p ) {
+  p.yearOfDeath ??= new Date().getFullYear();
+  return p.yearOfDeath - p.yearOfBirth;
+}
 
+const findTheOldest = function( p ) {
+  return p.reduce( 
+    ( oldestPerson, currentPerson ) => {
+      const oldestPersonAge = getAge( oldestPerson );
+      const currentPersonAge = getAge( currentPerson );
+      return oldestPersonAge < currentPersonAge ? currentPerson : oldestPerson;
+    }
+  );  
 };
 
 // Do not edit below this line
